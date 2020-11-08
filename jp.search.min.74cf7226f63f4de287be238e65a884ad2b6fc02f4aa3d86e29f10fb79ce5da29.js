7fad9835bd28e1a98b3d9cf30ded97c6b0ded1a0
shunkosa <shunkosa@users.noreply.github.com>
2020-11-08 06:49:59 +0000

    deploy: 3fa502ab04929f4cb99cb851bf5cb27e08ac5d0a

--- jp.search.min.74cf7226f63f4de287be238e65a884ad2b6fc02f4aa3d86e29f10fb79ce5da29.js
+++ jp.search.min.74cf7226f63f4de287be238e65a884ad2b6fc02f4aa3d86e29f10fb79ce5da29.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/lightning-flow-recipes-jp/flexsearch.min.js');loadScript('/lightning-flow-recipes-jp/jp.search-data.min.5a1c9e8847d6a1bc82feb1d91a63775382bf80415c6fa6e2f5247a41c689ac7e.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/lightning-flow-recipes-jp/flexsearch.min.js');loadScript('/lightning-flow-recipes-jp/jp.search-data.min.b88de9747fb0571e9991a01479bcd7ac0278b2afabfd077af022d5cefa2f99ba.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
